eba9270a63efa06fa7b2173a4e25d0add31f1620
Lee Zi Hong <zh.lee@worksmartly.com>
2021-12-14 16:50:39 +0800

    style(pages): update Login & Register view

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardContent,
@@ -11,8 +12,16 @@ import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
 import { Link } from "react-router-dom";
 import auth from "../authentication/firebase";
 import { useHistory } from "react-router";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import CheckCircleOutlineOutlinedIcon from "@mui/icons-material/CheckCircleOutlineOutlined";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 
-export default function Login() {
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function Login({ open, handleClose }) {
   // History to navigate back previous
   const history = useHistory();
 
@@ -45,144 +54,180 @@ export default function Login() {
 
   return (
     <div className="login">
-      {/* Card of login */}
-      <Card
+      <Snackbar
+        open={open}
+        autoHideDuration={1000}
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          padding: "0px",
-          width: "381px",
-          height: "381px",
-          left: "859px",
-          position: "absolute",
-          top: "50%",
-          transform: "translateY(-50%)",
-          background: "#FFFFFF",
-          boxShadow:
-            "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
+          width: "350px",
+          height: "80px",
           borderRadius: "20px",
+          background: "white",
         }}
       >
-        <CardHeader
-          title={
-            <Typography
-              sx={{
-                position: "absolute",
-                width: "87px",
-                height: "42px",
-                left: "147px",
-                top: "50px",
-                fontFamily: "Exo",
-                fontStyle: "normal",
-                fontWeight: "normal",
-                fontSize: "34px",
-                lineHeight: "123.5%",
-                display: "flex",
-                alignItems: "center",
-                letterSpacing: "0.25px",
-                color: "#000000",
-              }}
-            >
-              Login
-            </Typography>
-          }
-        />
+        <Box>
+          <Alert
+            icon={<CheckCircleOutlineOutlinedIcon sx={{ color: "#4cac54" }} />}
+            sx={{
+              width: "100%",
+              background: "white",
+              color: "#000000",
+              boxShadow: "none",
+            }}
+          >
+            Verification email sent.
+          </Alert>
+          <CancelOutlinedIcon
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "0px",
+              position: "absolute",
+              left: "88.57%",
+              right: "3.43%",
+              top: "15%",
+              bottom: "50%",
+              cursor: "pointer",
+            }}
+            onClick={handleClose}
+          />
+        </Box>
+      </Snackbar>
+      {/* Card of login */}
+      <Box sx={{ background: "#9ADCBB", width: "100vw", height: "100vh" }}>
+        {/* Card of register */}
+        <Card
+          sx={{
+            position: "absolute",
+            width: "381px",
+            height: "386px",
+            left: "calc(50% - 381px/2 - 0.5px)",
+            top: "calc(50% - 458px/2 + 8px)",
+            boxShadow:
+              "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
+            borderRadius: "20px",
+          }}
+        >
+          <CardHeader
+            title={
+              <Typography
+                sx={{
+                  position: "absolute",
+                  width: "87px",
+                  height: "42px",
+                  left: "147px",
+                  top: "50px",
+                  fontFamily: "Exo",
+                  fontStyle: "normal",
+                  fontWeight: "normal",
+                  fontSize: "34px",
+                  lineHeight: "123.5%",
+                  display: "flex",
+                  alignItems: "center",
+                  letterSpacing: "0.25px",
+                  color: "#000000",
+                }}
+              >
+                Login
+              </Typography>
+            }
+          />
 
-        <CardContent>
-          <form noValidate autoComplete="off">
-            <TextField
-              label="Username"
-              fullWidth
-              required
-              onChange={(e) => {
-                setemail(e.target.value);
-              }}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                padding: "0px",
-                position: "absolute",
-                width: "320px",
-                height: "56px",
-                left: "30px",
-                top: "134px",
-                textAlign: "center",
-              }}
-            />
-            <TextField
-              label="Password"
-              fullWidth
-              required
-              onChange={(e) => {
-                setpassword(e.target.value);
-              }}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                padding: "0px",
-                position: "absolute",
-                width: "320px",
-                height: "56px",
-                left: "30px",
-                top: "206px",
-              }}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              onClick={signIn}
-              sx={{
-                padding: "0px",
-                position: "absolute",
-                width: "320px",
-                height: "42px",
-                left: "30px",
-                top: "286px",
-                background: "#50D492",
-                boxShadow:
-                  "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
-                borderRadius: "10px",
-                "&:hover": {
-                  background: "#37BB79",
-                },
-              }}
-              endIcon={<ArrowForwardOutlinedIcon />}
-            >
-              SIGN IN
-            </Button>
-          </form>
+          <CardContent>
+            <form noValidate autoComplete="off">
+              <TextField
+                label="Username"
+                fullWidth
+                required
+                onChange={(e) => {
+                  setemail(e.target.value);
+                }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  padding: "0px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "56px",
+                  left: "30px",
+                  top: "134px",
+                  textAlign: "center",
+                }}
+              />
+              <TextField
+                label="Password"
+                fullWidth
+                required
+                onChange={(e) => {
+                  setpassword(e.target.value);
+                }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  padding: "0px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "56px",
+                  left: "30px",
+                  top: "206px",
+                }}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                onClick={signIn}
+                sx={{
+                  padding: "0px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "42px",
+                  left: "30px",
+                  top: "286px",
+                  background: "#50D492",
+                  boxShadow:
+                    "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
+                  borderRadius: "10px",
+                  "&:hover": {
+                    background: "#37BB79",
+                  },
+                }}
+                endIcon={<ArrowForwardOutlinedIcon />}
+              >
+                SIGN IN
+              </Button>
+            </form>
 
-          <Link to="/register">
-            <Typography
-              sx={{
-                fontFamily: "Exo",
-                fontStyle: "normal",
-                fontWeight: "normal",
-                lineHeight: "123.5%",
-                color: "#000000",
-                padding: "0px",
-                display: "flex",
-                alignItems: "center",
-                letterSpacing: "0.25px",
-                position: "absolute",
-                width: "320px",
-                height: "56px",
-                left: "73px",
-                top: "318px",
-                textAlign: "center",
-                "&:hover": {
-                  color: "#3366BB",
-                },
-              }}
-            >
-              Don't have an account yet? Sign Up
-            </Typography>
-          </Link>
-        </CardContent>
-      </Card>
+            <Link to="/register">
+              <Typography
+                sx={{
+                  fontFamily: "Exo",
+                  fontStyle: "normal",
+                  fontWeight: "normal",
+                  lineHeight: "123.5%",
+                  color: "#000000",
+                  padding: "0px",
+                  display: "flex",
+                  alignItems: "center",
+                  letterSpacing: "0.25px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "56px",
+                  left: "73px",
+                  top: "328px",
+                  textAlign: "center",
+                  "&:hover": {
+                    color: "#3366BB",
+                  },
+                }}
+              >
+                Not a user ? Register here
+              </Typography>
+            </Link>
+          </CardContent>
+        </Card>
+      </Box>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardContent,
@@ -12,172 +13,193 @@ import { Link } from "react-router-dom";
 import auth from "../authentication/firebase";
 import { useHistory } from "react-router";
 
-export default function Register() {
+export default function Register({ setOpen }) {
   // History to navigate back previous
   const history = useHistory();
 
   // Get user email and password
   const [email, setemail] = useState("");
+  const [confirmPassword, setconfirmpassword] = useState("");
   const [password, setpassword] = useState("");
 
   const signUp = (e) => {
     e.preventDefault();
-    if (email && password) {
-      auth
-        .createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-          // send verification mail.
-          userCredential.user.sendEmailVerification();
-          auth.signOut();
-          alert("Email sent, please verify it before logging in");
-          history.push("/login");
-        })
-        .catch(alert);
+
+    if (confirmPassword !== password) {
+      alert("Password set doesn't match!");
     } else {
-      alert("Please fill out all info stated");
+      if (email && password) {
+        auth
+          .createUserWithEmailAndPassword(email, password)
+          .then((userCredential) => {
+            // send verification mail.
+            userCredential.user.sendEmailVerification();
+            auth.signOut();
+            setOpen(true);
+            history.push("/login");
+          })
+          .catch(alert);
+      } else {
+        alert("Please fill out all info stated");
+      }
     }
   };
 
   return (
     <div className="register">
-      {/* Card of register */}
-      <Card
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          padding: "0px",
-          width: "381px",
-          height: "381px",
-          left: "859px",
-          position: "absolute",
-          top: "50%",
-          transform: "translateY(-50%)",
-          background: "#FFFFFF",
-          boxShadow:
-            "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
-          borderRadius: "20px",
-        }}
-      >
-        <CardHeader
-          title={
-            <Typography
-              sx={{
-                position: "absolute",
-                width: "87px",
-                height: "42px",
-                left: "137px",
-                top: "50px",
-                fontFamily: "Exo",
-                fontStyle: "normal",
-                fontWeight: "normal",
-                fontSize: "34px",
-                lineHeight: "123.5%",
-                display: "flex",
-                alignItems: "center",
-                letterSpacing: "0.25px",
-                color: "#000000",
-              }}
-            >
-              Register
-            </Typography>
-          }
-        />
+      <Box sx={{ background: "#9ADCBB", width: "100vw", height: "100vh" }}>
+        {/* Card of register */}
+        <Card
+          sx={{
+            position: "absolute",
+            width: "381px",
+            height: "458px",
+            left: "calc(50% - 381px/2 - 0.5px)",
+            top: "calc(50% - 458px/2 + 8px)",
+            boxShadow:
+              "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
+            borderRadius: "20px",
+          }}
+        >
+          <CardHeader
+            title={
+              <Typography
+                sx={{
+                  position: "absolute",
+                  width: "87px",
+                  height: "42px",
+                  left: "137px",
+                  top: "50px",
+                  fontFamily: "Exo",
+                  fontStyle: "normal",
+                  fontWeight: "normal",
+                  fontSize: "34px",
+                  lineHeight: "123.5%",
+                  display: "flex",
+                  alignItems: "center",
+                  letterSpacing: "0.25px",
+                  color: "#000000",
+                }}
+              >
+                Register
+              </Typography>
+            }
+          />
 
-        <CardContent>
-          <form noValidate autoComplete="off">
-            <TextField
-              label="Username"
-              fullWidth
-              required
-              onChange={(e) => {
-                setemail(e.target.value);
-              }}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                padding: "0px",
-                position: "absolute",
-                width: "320px",
-                height: "56px",
-                left: "30px",
-                top: "134px",
-                textAlign: "center",
-              }}
-            />
-            <TextField
-              label="Password"
-              fullWidth
-              required
-              onChange={(e) => {
-                setpassword(e.target.value);
-              }}
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                padding: "0px",
-                position: "absolute",
-                width: "320px",
-                height: "56px",
-                left: "30px",
-                top: "206px",
-              }}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              onClick={signUp}
-              sx={{
-                padding: "0px",
-                position: "absolute",
-                width: "320px",
-                height: "42px",
-                left: "30px",
-                top: "286px",
-                background: "#50D492",
-                boxShadow:
-                  "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
-                borderRadius: "10px",
-                "&:hover": {
-                  background: "#37BB79",
-                },
-              }}
-              endIcon={<ArrowForwardOutlinedIcon />}
-            >
-              REGISTER
-            </Button>
-          </form>
+          <CardContent>
+            <form noValidate autoComplete="off">
+              <TextField
+                label="Username"
+                fullWidth
+                required
+                onChange={(e) => {
+                  setemail(e.target.value);
+                }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  padding: "0px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "56px",
+                  left: "30px",
+                  top: "134px",
+                  textAlign: "center",
+                }}
+              />
+              <TextField
+                label="Password"
+                fullWidth
+                required
+                onChange={(e) => {
+                  setpassword(e.target.value);
+                }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  padding: "0px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "56px",
+                  left: "30px",
+                  top: "206px",
+                }}
+              />
+              <TextField
+                label="Confirm Password"
+                fullWidth
+                required
+                onChange={(e) => {
+                  setconfirmpassword(e.target.value);
+                }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  padding: "0px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "56px",
+                  left: "30px",
+                  top: "278px",
+                }}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                onClick={signUp}
+                sx={{
+                  padding: "0px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "42px",
+                  left: "30px",
+                  top: "358px",
+                  background: "#50D492",
+                  boxShadow:
+                    "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
+                  borderRadius: "10px",
+                  "&:hover": {
+                    background: "#37BB79",
+                  },
+                }}
+                endIcon={<ArrowForwardOutlinedIcon />}
+              >
+                SIGN UP
+              </Button>
+            </form>
 
-          <Link to="/login">
-            <Typography
-              sx={{
-                fontFamily: "Exo",
-                fontStyle: "normal",
-                fontWeight: "normal",
-                lineHeight: "123.5%",
-                color: "#000000",
-                padding: "0px",
-                display: "flex",
-                alignItems: "center",
-                letterSpacing: "0.25px",
-                position: "absolute",
-                width: "320px",
-                height: "56px",
-                left: "73px",
-                top: "318px",
-                textAlign: "center",
-                "&:hover": {
-                  color: "#3366BB",
-                },
-              }}
-            >
-              Have an account already? Sign In
-            </Typography>
-          </Link>
-        </CardContent>
-      </Card>
+            <Link to="/login">
+              <Typography
+                sx={{
+                  fontFamily: "Exo",
+                  fontStyle: "normal",
+                  fontWeight: "normal",
+                  lineHeight: "123.5%",
+                  color: "#000000",
+                  padding: "0px",
+                  display: "flex",
+                  alignItems: "center",
+                  letterSpacing: "0.25px",
+                  position: "absolute",
+                  width: "320px",
+                  height: "56px",
+                  left: "73px",
+                  top: "400px",
+                  textAlign: "center",
+                  "&:hover": {
+                    color: "#3366BB",
+                  },
+                }}
+              >
+                Already a user? Login here
+              </Typography>
+            </Link>
+          </CardContent>
+        </Card>
+      </Box>
     </div>
   );
 }
